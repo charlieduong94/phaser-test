3e1cdbbb32a20c3b244a8aa693e44279e04ce8a0
Charlie Duong <charlieduong94@gmail.com>
2017-12-21 18:53:41 -0500

    Remove bundle

--- src/game.js
+++ src/game.js
@@ -6,12 +6,21 @@ const { Game } = Phaser
 
 const game = new Game(800, 600, Phaser.CANVAS, 'phaser-test', {
   preload () {
-    console.log('preloaded')
     game.load.image('cat', './assets/persona5_cat.jpg')
+    game.load.image('coin', './assets/coin.png')
   },
 
   create () {
-    console.log('created')
-    const sprite = game.add.sprite(80, 0, 'cat');
+    // const sprite = game.add.sprite(80, 0, 'cat');
+    const coinSprite = game.add.sprite(80, 0, 'coin')
+
+    // simple tween animation
+    const tween = game.add.tween(coinSprite)
+    tween.to({ y: 100 }, 1000, Phaser.Easing.Linear.None, true)
+      .to({ y: 0 }, 1000, Phaser.Easing.Linear.None, true)
+
+    tween.start()
+
+    // tween.loop(true)
   },
 });
--- src/globals.js
+++ src/globals.js
@@ -3,9 +3,9 @@
  *
  * This needs to be done to properly use Phaser
  */
-window.PIXI = require('phaser/build/custom/pixi')
-window.p2 = require('phaser/build/custom/p2')
-window.Phaser = require('phaser/build/custom/phaser-split')
+window.PIXI = require('phaser-ce/build/custom/pixi')
+window.p2 = require('phaser-ce/build/custom/p2')
+window.Phaser = require('phaser-ce/build/custom/phaser-split')
 
 module.exports = {
   PIXI,
